acc06015f57bce97b1bd46997927ee2360418882
Makaradhwaja <makaradhwajabehera17235@gmail.com>
2020-10-29 13:23:43 +0530

    publishing my personal website on github

---
+++ README.md
@@ -0,0 +1,21 @@
+# My portfolio website project
+This is  a pure html, css and javascript based website design by me. This is completely responsive and fit on any devives.
+### one screenshot
+![screenshot1](assets/img/Screenshot1.png)
+___
+### responsive screenshot
+![screenshot1](assets/img/Screenshot2.png)
+___
+This is completely responsive and functioning website. Here the given contact form is working properly, I mean after filling the data when we pressed submit the data will sent to my email address.
+* I have used API call to send the data to my email.
+___
+
+In this website I have described myself with smooth effects and so many futuristic components.
+
+Technologies used:-
+* HTML5
+* CSS3
+* javascript
+* API call to send data to my email
+
+
---
+++ assets/js/main.js
@@ -0,0 +1,107 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    /*Remove menu mobile*/
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title', {});
+sr.reveal('.button', { delay: 200 });
+sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', {});
+sr.reveal('.about__subtitle', { delay: 400 });
+sr.reveal('.about__text', { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {});
+sr.reveal('.skills__data', { interval: 200 });
+sr.reveal('.skills__img', { delay: 600 });
+
+/*SCROLL WORK*/
+sr.reveal('.work__img', { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input', { interval: 200 });
+
+// ====== Form Submision========
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thanks! submitted successfully";
+    }
+
+    function error() {
+      status.innerHTML = "Oops! There was a problem. Try Again";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+
+ 
